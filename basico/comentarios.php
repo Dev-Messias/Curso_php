a586c64863cae1f67f1a17af5b597cac2539f035
DevMessias <messiasbento944@gmail.com>
2022-08-16 10:15:00 -0300

    Comentarios PHP

---
+++ basico/comentarios.php
@@ -0,0 +1,17 @@
+<div class="titulo" >Comentarios PHP</div>
+
+<?php 
+echo "Estou no PHP";
+//echo "estou no PHP";
+# echo "estou no PHP";
+
+?>
+
+<p>Depois do primeiro bloco!</p>
+
+<?php 
+    /**
+     * Linha 1 do comentario
+     * Linha 2 do comentario
+     */
+?>
