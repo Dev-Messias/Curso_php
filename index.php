01a17cf269abd11e85c3cc2789e2c5f87989bf03
DevMessias <messiasbento944@gmail.com>
2022-08-16 10:26:08 -0300

    Desafio 01

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
               <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
               <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
               <li><a href="exercicio.php?dir=basico&file=comentarios">Comentarios PHP</a></li>
+              <li><a href="exercicio.php?dir=basico&file=desafio">Desafio 01</a></li>
             </ul>
           </div>
 
