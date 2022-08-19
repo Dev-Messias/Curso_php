b18d9695ce47d992cff9d2dfb4f30314b5101313
DevMessias <messiasbento944@gmail.com>
2022-08-18 21:49:22 -0300

    Desafio_String

---
+++ tipos/desafio_string.php
@@ -0,0 +1,15 @@
+<div class="titulo">Desafio String</div>
+
+<?php 
+
+/**
+ * Enuciado:
+ * Avaliando os métodos da documentação da string,
+ * qual o método que a posição do teste 'abc'
+ * na string '!AbcaBcabc' retorna 1?
+ */
+
+ echo strpos('!AbcaBcabc', 'abc') . '<br>';
+ echo stripos('!AbcaBcabc', 'abc') . '<br>';
+
+?>
