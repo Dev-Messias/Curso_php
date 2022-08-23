973c507a4806c60af9175ad1e2de917a25dc6774
DevMessias <messiasbento944@gmail.com>
2022-08-23 20:58:07 -0300

    Constante

--- index.php
+++ index.php
@@ -53,6 +53,7 @@
               <li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
               <li><a href="exercicio.php?dir=variaveis&file=atribuicao">Atribuição</a></li>
               <li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
+              <li><a href="exercicio.php?dir=variaveis&file=constante">Constante</a></li>
             </ul>
           </div>
 
---
+++ variaveis/constante.php
@@ -0,0 +1,14 @@
+<div class="titulo">Constante</div>
+
+<?php 
+
+    define('TAXA_DE_JUROS', 5.9);
+    echo TAXA_DE_JUROS;
+    echo '<br>' . $TAXA_DE_JUROS . '!';
+    
+
+    const NOVA_TAXA = 2.5;
+
+    echo '<br>' . NOVA_TAXA;
+
+?>
